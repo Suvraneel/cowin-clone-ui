ca6585d9296aa64d8d1d18dcc9e0a6c852d78bb5
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-02-14 18:51:21 +0530

    JSSoup fetches current dose count from official site

--- src/pages/home/components/counter.jsx
+++ src/pages/home/components/counter.jsx
@@ -1,33 +1,38 @@
 import React, { Component } from "react";
 import "./styles/counter.css";
-
+import JSSoup from 'jssoup';
+import CountUp from 'react-countup';
 class Counter extends React.Component {
   componentDidMount() {
-    function numberWithCommas(x) {
-      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
-
-    var seconds = 0;
-    var totalDoses = 1518498562;
-    var todayDoses = 2507890;
-    var totalElt = document.getElementById("total-doses");
-    var todayElt = document.getElementById("today-doses");
-
-    function incrementNumber() {
-      if (seconds % 2 === 0) {
+    var seconds = 0, totalDoses = 0, todayDoses = 0;
+    var totalElem = document.getElementById("total-doses");
+    var todayElem = document.getElementById("today-doses");
+    var soup;
+    // JSSoup is a library that parses HTML and CSS (similar to BeautifulSoup)
+    fetch('https://www.mohfw.gov.in/')
+    .then(data => data.text())
+    .then(data => {
+      // console.log(data);
+      soup = new JSSoup(data);
+      // console.log(soup.prettify());
+      var elements = soup.findAll('span', 'coviddata')
+      console.log(elements);
+      var total = elements[0].text.trim();
+      var today = elements[0].nextSibling.text.trim();
+      // console.log(total, today, today.substring(1, today.length - 1));
+      totalDoses = parseInt(total.replace(/,/g, ''));
+      todayDoses = parseInt(today.substring(1, today.length - 1).replace(/,/g, ''));
+      console.log(totalDoses, todayDoses);
+    });
+    
+    setInterval(()=>{
+      if (seconds % 3 === 0) {
         totalDoses += 1;
         todayDoses += 1;
-        totalElt.innerText = numberWithCommas(totalDoses);
-        todayElt.innerText = numberWithCommas(todayDoses);
-      }
-    }
-
-    function incrementSeconds() {
-      seconds += 1;
-    }
-
-    var tik = setInterval(incrementNumber, 10);
-    var tok = setInterval(incrementSeconds, 1000);
+        totalElem.innerText = totalDoses.toLocaleString('en-IN');
+        todayElem.innerText = todayDoses.toLocaleString('en-IN');
+    }}, Math.random() * 50 + 100);
+    setInterval(()=>{seconds += 1}, Math.random() * 100 + 500);
   }
 
   render() {
@@ -43,8 +48,16 @@ class Counter extends React.Component {
                 <div className="col">
                   <div className="row title">Total Vaccination Doses</div>
                   <div id="total-doses" className="row number">
-                    1,518,498,562
+                    <CountUp
+                    end={1700000000}
+                    duration="10"
+                  />
                   </div>
+                  {/* <CountUp
+                    start={100000}
+                    end={123457}
+                    duration="10000"
+                  /> */}
                 </div>
               </div>
             </div>
@@ -52,9 +65,12 @@ class Counter extends React.Component {
             <div className="col-md counter-col">
               <div className="row">
                 <div className="col">
-                  <div className="row title">Total Vaccination Doses</div>
+                  <div className="row title">Vaccinations Done Today</div>
                   <div id="today-doses" className="row number">
-                    2,507,890
+                  <CountUp
+                    end={5000000}
+                    duration="10"
+                  />
                   </div>
                 </div>
               </div>
